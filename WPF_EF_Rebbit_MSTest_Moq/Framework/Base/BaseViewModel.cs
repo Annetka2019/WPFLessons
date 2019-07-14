2125de4d9ed025f9b12081e7a842e73f64da42c5
Pecheroga <Pecheroga@gmail.com>
2019-07-14 23:22:18 +0300

    Add CardSectionViewModel

---
+++ WPF_EF_Rebbit_MSTest_Moq/Framework/Base/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WPF_EF_Rebbit_MSTest_Moq.Annotations;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Framework.Base
+{
+	public class BaseViewModel: INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ WPF_EF_Rebbit_MSTest_Moq/Project/ViewModel/CardPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_EF_Rebbit_MSTest_Moq.Framework.Base;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Project.ViewModel
+{
+	public class CardPageViewModel: BaseViewModel
+	{
+	}
+}

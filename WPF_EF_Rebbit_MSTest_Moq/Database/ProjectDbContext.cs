9a591a4bff25ce07d8533449a06692949b990bef
Pecheroga <Pecheroga@gmail.com>
2019-07-14 22:49:07 +0300

    Add Card and Indicator

---
+++ WPF_EF_Rebbit_MSTest_Moq/Database/ProjectDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using WPF_EF_Rebbit_MSTest_Moq.Project.Model;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Database
+{
+	public class ProjectDbContext: DbContext
+    {
+	    public ProjectDbContext(string connectionString = "db"): base(connectionString) {}
+
+	    public DbSet<Card> Cards { get; set; }
+
+		public DbSet<Indicator> Indicators { get; set; }
+    }
+}
---
+++ WPF_EF_Rebbit_MSTest_Moq/Project/Model/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Project.Model
+{
+    public class Card
+    {
+	    public Guid Id { get; set; }
+
+		[Required]
+	    public string Number { get; set; }
+
+	    [Required]
+		public string Name { get; set; }
+
+	    public DateTime? StartDate { get; set; }
+
+	    public DateTime? DueDate { get; set; }
+
+	    public decimal Assessment { get; set; }
+
+	    public virtual ICollection<Indicator> Indicators { get; set; } = new HashSet<Indicator>();
+    }
+}

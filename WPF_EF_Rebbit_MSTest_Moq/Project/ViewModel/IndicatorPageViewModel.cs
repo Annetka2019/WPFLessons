51b05771227f392add86963c21133696ade7d112
Pecheroga <Pecheroga@gmail.com>
2019-07-20 21:38:17 +0300

    Added Indicator section and page view and viewModel

---
+++ WPF_EF_Rebbit_MSTest_Moq/Project/ViewModel/IndicatorPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Windows.Input;
+using WPF_EF_Rebbit_MSTest_Moq.Database;
+using WPF_EF_Rebbit_MSTest_Moq.Framework;
+using WPF_EF_Rebbit_MSTest_Moq.Framework.Base;
+using WPF_EF_Rebbit_MSTest_Moq.Project.Model;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Project.ViewModel
+{
+	public class IndicatorPageViewModel: BaseViewModel
+	{
+		public IndicatorPageViewModel() {
+			InitCommands();
+		}
+
+		public Indicator Indicator { get; set; }
+
+		public ICommand SaveCommand { get; private set; }
+
+		private void InitCommands() {
+			SaveCommand = new DelegateCommand(SaveIndicator, CanSaveIndicator);
+		}
+
+		private void SaveIndicator(object obj) {
+			using (var ctx = new ProjectDbContext()) {
+				var indicatorDb = ctx.Indicators.FirstOrDefault(i => i.Id == Indicator.Id);
+				if (indicatorDb != null) {
+					indicatorDb.Name = Indicator.Name;
+					indicatorDb.Type = Indicator.Type;
+				}
+				else {
+					ctx.Indicators.Add(Indicator);
+				}
+				ctx.SaveChanges();
+			}
+		}
+
+		private bool CanSaveIndicator(object obj) {
+			return Indicator.Name != string.Empty && Indicator.Type != string.Empty;
+		}
+	}
+}
---
+++ WPF_EF_Rebbit_MSTest_Moq/Project/ViewModel/IndicatorSectionViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_EF_Rebbit_MSTest_Moq.Database;
+using WPF_EF_Rebbit_MSTest_Moq.Framework;
+using WPF_EF_Rebbit_MSTest_Moq.Framework.Base;
+using WPF_EF_Rebbit_MSTest_Moq.Project.Model;
+using WPF_EF_Rebbit_MSTest_Moq.Project.View;
+
+namespace WPF_EF_Rebbit_MSTest_Moq.Project.ViewModel
+{
+	public class IndicatorSectionViewModel: BaseViewModel
+	{
+		public IndicatorSectionViewModel() {
+			InitIndicatorsCollection();
+			InitCommands();
+		}
+
+		public ObservableCollection<Indicator> Indicators { get; private set; }
+		public Indicator  SelectedIndicator { get; set; }
+		public ICommand AddCommand { get; private set; }
+		public ICommand EditCommand { get; private set; }
+
+		private void InitIndicatorsCollection() {
+			using (var ctx = new ProjectDbContext()) {
+				Indicators = new ObservableCollection<Indicator>(ctx.Indicators);
+			}
+		}
+
+		private void InitCommands() {
+			AddCommand = new DelegateCommand(AddIndicator, CanAddIndicator);
+			EditCommand = new DelegateCommand(EditIndicator, CanEditIndicator);
+		}
+
+		private void AddIndicator(object obj) {
+			var indicatorPage = new IndicatorPage();
+			var indicatorPageViewModel = new IndicatorPageViewModel();
+			indicatorPage.DataContext = indicatorPageViewModel;
+			indicatorPage.ShowDialog();
+		}
+
+		private bool CanAddIndicator(object obj) {
+			return true;
+		}
+
+		private void EditIndicator(object obj) {
+			var indicatorPage = new IndicatorPage();
+			var indicatorPageViewModel = new IndicatorPageViewModel { Indicator = SelectedIndicator};
+			indicatorPage.DataContext = indicatorPageViewModel;
+			indicatorPage.ShowDialog();
+		}
+
+		private bool CanEditIndicator(object obj) {
+			return SelectedIndicator != null;
+		}
+
+	}
+}
